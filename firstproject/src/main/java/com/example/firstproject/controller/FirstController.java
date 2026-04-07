6731ae921115101b7e21fe32f24efc282f5129d2
yeomdoyun <57949304+dose1203@users.noreply.github.com>
2026-04-07 18:48:35 +0900

    0401 spring

---
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,25 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.ui.Model;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model){
+
+        model.addAttribute("username","염도윤");
+        return "greetings";
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model){
+        model.addAttribute("nickname","홍길동");
+        return "goodbye";
+    }
+}
