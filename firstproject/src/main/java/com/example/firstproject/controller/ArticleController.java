95131b97e33fb436954c2ab7520eacd571f4cbce
yeomdoyun <57949304+dose1203@users.noreply.github.com>
2026-05-12 17:02:54 +0900

    0506 Spring

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,12 +1,19 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping; // GET 요청 처리
 import org.springframework.web.bind.annotation.PostMapping; // POST 요청 처리
+import com.example.firstproject.repository.ArticleRepository;
+
 
 @Controller //컨트롤러로 관리
 public class ArticleController {
+    @Autowired
+    private ArticleRepository articleRepository;
+
     @GetMapping("/articles/new")
     public String newArticleForm(){
         return "articles/new";
@@ -15,7 +22,13 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
         System.out.println(form.toString()); //다시 작성 페이지로 이동
+        Article article = form.toEntity();
+        System.out.println(article.toString());
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
         return "/articles/new"; // 다시 작성 페이지로 이동
     }
+
+
 
 }
